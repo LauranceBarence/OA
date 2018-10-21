0f82154530ef8dba91341d381b6956a2fe91ef94
LauranceBarence <823271383@qq.com>
2018-10-22 05:47:04 +0800

    finishi modules

--- src/main/java/com/bwf/entity/Operate.java
+++ src/main/java/com/bwf/entity/Operate.java
@@ -1,5 +1,46 @@
 package com.bwf.entity;
 
 public class Operate {
-
+	private Integer operateId;
+	private String operateName;
+	private String operateCode;
+	private String operateUrl;
+	private Integer pId;
+	public Integer getOperateId() {
+		return operateId;
+	}
+	public void setOperateId(Integer operateId) {
+		this.operateId = operateId;
+	}
+	public String getOperateName() {
+		return operateName;
+	}
+	public void setOperateName(String operateName) {
+		this.operateName = operateName;
+	}
+	public String getOperateCode() {
+		return operateCode;
+	}
+	public void setOperateCode(String operateCode) {
+		this.operateCode = operateCode;
+	}
+	public String getOperateUrl() {
+		return operateUrl;
+	}
+	public void setOperateUrl(String operateUrl) {
+		this.operateUrl = operateUrl;
+	}
+	
+	public Integer getpId() {
+		return pId;
+	}
+	public void setpId(Integer pId) {
+		this.pId = pId;
+	}
+	@Override
+	public String toString() {
+		return "Operate [operateId=" + operateId + ", operateName=" + operateName + ", operateCode=" + operateCode
+				+ ", operateUrl=" + operateUrl + "]";
+	}
+	
 }
